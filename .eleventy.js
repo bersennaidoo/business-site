bedd456db390caca292286bd4d3b4de05eb67850
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-06-03 11:46:55 +0200

    Extracted tw-elements.js from node modules

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("node_modules");
   return {
     dir: {
       input: "src",
